113585e13e58acea4543a671eb087e2d346b096e
Egzon Berisha <=egzon.berisha.9696@gmail.com>
2019-11-15 10:56:33 +0000

    15/11/2019

--- SpringBootJwtAuthentication/src/main/java/com/berisha/jwtauthentication/controller/UserController.java
+++ SpringBootJwtAuthentication/src/main/java/com/berisha/jwtauthentication/controller/UserController.java
@@ -4,10 +4,12 @@ import com.berisha.jwtauthentication.model.Todo;
 import com.berisha.jwtauthentication.model.User;
 import com.berisha.jwtauthentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -34,6 +36,16 @@ public class UserController {
         return ResponseEntity.noContent().build();
 
     }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUserRole(
+            @PathVariable long id, @RequestBody User user){
+
+            User updatedUser = userRepository.save(user);
+        return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
+    }
+
+
 
 
 }
--- SpringBootJwtAuthentication/src/main/java/com/berisha/jwtauthentication/security/WebSecurityConfig.java
+++ SpringBootJwtAuthentication/src/main/java/com/berisha/jwtauthentication/security/WebSecurityConfig.java
@@ -52,12 +52,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+            // other public endpoints of your API may be appended to this array
+    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
+
+                .antMatchers("/api/auth/**","/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
+
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
