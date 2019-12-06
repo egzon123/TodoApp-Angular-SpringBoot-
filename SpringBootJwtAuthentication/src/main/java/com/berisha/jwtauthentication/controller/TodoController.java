5540dd8777421520650dea00572bde73e73696a0
Floriaan <f.hoti@live.com>
2019-12-06 10:08:53 +0100

    Todo Controller Changes

--- SpringBootJwtAuthentication/src/main/java/com/berisha/jwtauthentication/controller/TodoController.java
+++ SpringBootJwtAuthentication/src/main/java/com/berisha/jwtauthentication/controller/TodoController.java
@@ -69,6 +69,12 @@ public class TodoController {
 		
 		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
 	}
+
+//	@PostMapping("/createTodo")
+//	public ResponseEntity<Todo> createTodo(@RequestBody Todo todo){
+//		todoJpaRepository.save(todo);
+//		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+//	}
 	
 	@PostMapping("/users/{username}/todos")
 	public ResponseEntity<Void> createTodo(
